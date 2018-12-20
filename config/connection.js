f2a7abdc5f927fa25d2266d3ca2586642d3e44a3
Asara <asarakumarasena94@gmail.com>
2018-12-20 19:40:43 +0530

    add route files and change connection js file

--- config/connection.js
+++ config/connection.js
@@ -6,56 +6,77 @@ var moment = require('moment');
 var db;
 var settings = {
 
-   host: 'localhost',
-   user: 'root',
-   password: '',
-   database: 'calendar_app'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'calendar_app'
 
 };
 
 function ConnectDatabase() {
-   if (!db) {
+    if (!db) {
 
-       db = mysql.createConnection(settings);
+        db = mysql.createConnection(settings);
 
-       db.connect(function (err) {
+        db.connect(function (err) {
 
-           if(!err){
-               console.log('database is connected');
-                 test();
-            
-           }
-           else{
-               console.log('database is not connected!!!');
-           }
-       })
+            if (!err) {
+                console.log('database is connected');
+                count_records();
 
-   }
-   return db;
+            }
+            else {
+                console.log('database is not connected!!!');
+            }
+        })
+
+    }
+    return db;
 }
 
+var count = 'SELECT COUNT(id) as total FROM horoscopes';
+var deletion = 'DELETE FROM horoscopes';
+
+function count_records(req, res, next) {
+    db.query(count, function (err, result) {
+        if (err) throw err;
+        if (result[0].total != 0) {
+            db.query(deletion, function (err, res) {
+                if (err) throw err;
+                test();
+            })
+        }
+        else {
+            test();
+        }
+
+    });
+}
+
+
 function test(req, res, next) {
-    var setDate=new Date("2018-12-20");
+    var setDate = new Date("2018-12-20");
+
     function msToTime(duration) {
         var milliseconds = parseInt((duration % 1000) / 100),
-          seconds = parseInt((duration / 1000) % 60),
-          minutes = parseInt((duration / (1000 * 60)) % 60),
-          hours = parseInt((duration / (1000 * 60 * 60)) % 24);
-      
+            seconds = parseInt((duration / 1000) % 60),
+            minutes = parseInt((duration / (1000 * 60)) % 60),
+            hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
         seconds = (seconds < 10) ? "0" + seconds : seconds;
-      
+
         return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
-      }
-    
-    for(var i=0; i<400; i++){
-        var today=new Date(setDate.getTime()+(1000*60*60*24*i));
-        var todaySunCalc=SunCalc.getTimes( today,6.927079,79.861244);
-        var sunrise=todaySunCalc.sunrise.getTime();
-        var sunset=todaySunCalc.sunset.getTime();
+    }
+
+    for (var i = 0; i < 400; i++) {
+        var today = new Date(setDate.getTime() + (1000 * 60 * 60 * 24 * i));
+        var todaySunCalc = SunCalc.getTimes(today, 6.927079, 79.861244);
+        var sunrise = todaySunCalc.sunrise.getTime();
+        var sunset = todaySunCalc.sunset.getTime();
         var difference = sunset - sunrise;
-        var eighthPotion = difference/8;
+        var eighthPotion = difference / 8;
         // switch
         var dayName = moment(today).format('dddd');
         var day;
@@ -66,90 +87,91 @@ function test(req, res, next) {
                 day = "Sunday";
                 subaDishawa = "dakuna"
                 maruDishawa = "uthura"
-                var rahuStart = sunrise + (eighthPotion*7);
-                var rahuEnd = sunrise + (eighthPotion*8);              
+                var rahuStart = sunrise + (eighthPotion * 7);
+                var rahuEnd = sunrise + (eighthPotion * 8);
                 break;
             case 1:
                 day = "Monday";
                 subaDishawa = "ginikona"
-                maruDishawa = "wayaba"                
-                var rahuStart = sunrise + (eighthPotion*1);
-                var rahuEnd = sunrise + (eighthPotion*2);              
+                maruDishawa = "wayaba"
+                var rahuStart = sunrise + (eighthPotion * 1);
+                var rahuEnd = sunrise + (eighthPotion * 2);
                 break;
             case 2:
                 day = "Tuesday";
                 subaDishawa = "nagenahira"
                 maruDishawa = "batahira"
-                var rahuStart = sunrise + (eighthPotion*6);
-                var rahuEnd = sunrise + (eighthPotion*7);              
+                var rahuStart = sunrise + (eighthPotion * 6);
+                var rahuEnd = sunrise + (eighthPotion * 7);
                 break;
             case 3:
                 day = "Wednesday";
                 subaDishawa = "iisana"
-                maruDishawa = "niritha"                
-                var rahuStart = sunrise + (eighthPotion*4);
-                var rahuEnd = sunrise + (eighthPotion*5);              
+                maruDishawa = "niritha"
+                var rahuStart = sunrise + (eighthPotion * 4);
+                var rahuEnd = sunrise + (eighthPotion * 5);
                 break;
             case 4:
                 day = "Thursday";
                 subaDishawa = "uthura"
-                maruDishawa = "dakuna"                
-                var rahuStart = sunrise + (eighthPotion*5);
-                var rahuEnd = sunrise + (eighthPotion*6);              
+                maruDishawa = "dakuna"
+                var rahuStart = sunrise + (eighthPotion * 5);
+                var rahuEnd = sunrise + (eighthPotion * 6);
                 break;
             case 5:
                 day = "Friday";
                 subaDishawa = "wayaba"
-                maruDishawa = "ginikona"                
-                var rahuStart = sunrise + (eighthPotion*3);
-                var rahuEnd = sunrise + (eighthPotion*4);              
+                maruDishawa = "ginikona"
+                var rahuStart = sunrise + (eighthPotion * 3);
+                var rahuEnd = sunrise + (eighthPotion * 4);
                 break;
             case 6:
                 day = "Saturday";
                 subaDishawa = "batahira"
-                maruDishawa = "nagenahira"                
-                var rahuStart = sunrise + (eighthPotion*2);
-                var rahuEnd = sunrise + (eighthPotion*3);                            
-          }
+                maruDishawa = "nagenahira"
+                var rahuStart = sunrise + (eighthPotion * 2);
+                var rahuEnd = sunrise + (eighthPotion * 3);
+        }
         //Writing to Database
-          var todayDate =moment(today).format('L').toString(); 
-          var todaySunrise = msToTime(sunrise +1.98e+7).toString(); //+0530 GMT = +1.98e+7
-          var todaySunset = msToTime(sunset +1.98e+7).toString(); //+0530 GMT = +1.98e+7
-          var todayRahuMorningStart = msToTime(rahuStart + 1.98e+7).toString();
-          var todayRahuMorningEnd = msToTime(rahuEnd + 1.98e+7).toString();
-          var todayRahuNightStart = msToTime(rahuStart + 1.98e+7 + 4.32e+7).toString(); //12h = 4.32e+7ms
-          var todayRahuNightEnd = msToTime(rahuEnd + 1.98e+7 + 4.32e+7).toString();
-          console.log('----------------')
-          console.log('Date')
-          console.log(todayDate)
-          console.log('Sunrise')
-          console.log(todaySunrise)
-          console.log('Sunset')
-          console.log(todaySunset)
-          console.log('todayRahuMorningStart')
-          console.log(todayRahuMorningStart)
-          console.log('todayRahuMorningEnd')
-          console.log(todayRahuMorningEnd)
-          console.log('------')
-          console.log('todayRahuNightStart')
-          console.log(todayRahuNightStart)
-          console.log('todayRahuNightEnd')
-          console.log(todayRahuNightEnd)
-          console.log('-------')
-          console.log('subaDishawa')
-          console.log(subaDishawa)
-          console.log('maruDishawa')
-          console.log(maruDishawa)
-          console.log('----------------')
-          console.log('|||||||||||||||||')
-          console.log('----------------')
+        var todayDate = moment(today).format('YYYY-MM-DD');
+        var todaySunrise = msToTime(sunrise + 1.98e+7).toString(); //+0530 GMT = +1.98e+7
+        var todaySunset = msToTime(sunset + 1.98e+7).toString(); //+0530 GMT = +1.98e+7
+        var todayRahuMorningStart = msToTime(rahuStart + 1.98e+7).toString();
+        var todayRahuMorningEnd = msToTime(rahuEnd + 1.98e+7).toString();
+        var todayRahuNightStart = msToTime(rahuStart + 1.98e+7 + 4.32e+7).toString(); //12h = 4.32e+7ms
+        var todayRahuNightEnd = msToTime(rahuEnd + 1.98e+7 + 4.32e+7).toString();
+        /* console.log('----------------')
+         console.log('Date')
+         console.log(todayDate)
+         console.log('Sunrise')
+         console.log(todaySunrise)
+         console.log('Sunset')
+         console.log(todaySunset)
+         console.log('todayRahuMorningStart')
+         console.log(todayRahuMorningStart)
+         console.log('todayRahuMorningEnd')
+         console.log(todayRahuMorningEnd)
+         console.log('------')
+         console.log('todayRahuNightStart')
+         console.log(todayRahuNightStart)
+         console.log('todayRahuNightEnd')
+         console.log(todayRahuNightEnd)
+         console.log('-------')
+         console.log('subaDishawa')
+         console.log(subaDishawa)
+         console.log('maruDishawa')
+         console.log(maruDishawa)
+         console.log('----------------')
+         console.log('|||||||||||||||||')
+         console.log('----------------')*/
 
-          
 
         var sql = `INSERT INTO horoscopes (date, sunrise, sunset, rahu_morning_start, rahu_morning_end, rahu_night_start, rahu_night_end, suba_dishawa, maru_dishawa) VALUES ('${todayDate}', '${todaySunrise}', '${todaySunset}', '${todayRahuMorningStart}','${todayRahuMorningEnd}','${todayRahuNightStart}', '${todayRahuNightEnd}', '${subaDishawa}', '${maruDishawa}')`;
-        db.query(sql,function(err,raw){
-            if(err) throw err;
-        })
+        db.query(sql, function (err, raw) {
+            if (err) throw err;
+        });
+
     }
-}    
-module.exports=ConnectDatabase();
+}
+
+module.exports = ConnectDatabase();
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,84 @@
 var express = require('express');
 var router = express.Router();
-var connection=require('../config/connection');
-var SunCalc = require('suncalc');
+var connection = require('../config/connection');
+var mysql = require('mysql');
+var ArrayList = require('arraylist')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home/:date', function (req, res, next) {
 
+    var date = req.params.date;
+    var get_all = 'SELECT * FROM horoscopes WHERE date=' + mysql.escape(date);
+
+    connection.query(get_all, function (err, result) {
+        if (err) throw err;
+        var special_date_id = result[0].special_date_id;
+
+        if (special_date_id != 1) {
+            var get_special_date = 'SELECT description,image_url FROM special_dates WHERE id=' + mysql.escape(special_date_id);
+        }
+        else {
+            var get_special_date = 'SELECT description,image_url FROM special_dates WHERE id=1';
+        }
+        connection.query(get_special_date, function (err, rows) {
+            if (err) throw err;
+            res.json({type: true, rows, result});
+        })
+
+    });
+
+});
+
+router.get('/compass/:date', function (req, res) {
+    var date = req.params.date;
+
+    var get_disha = 'SELECT suba_dishawa,maru_dishawa FROM horoscopes WHERE date=' + mysql.escape(date);
+    connection.query(get_disha, function (err, result) {
+        if (err) throw err;
+        res.json({type: true, result})
+    })
+
+});
+
+router.get('/calender/:year/:month', function (req, res) {
+
+    var year = req.params.year;
+    var month = req.params.month;
+    var dates='SELECT category_id,day(date) as day FROM horoscopes WHERE month(date)=? AND year(date)=?';
+
+    connection.query(dates,[month,year],function (err,result) {
+        if(err)throw err;
+        res.json({type: true,result});
+    });
+});
+
+router.get('/special_dates/:year/:ln',function (req,res) {
+
+    var year = req.params.year;
+    var language = req.params.ln;
+
+    var years='SELECT special_date_id FROM horoscopes WHERE year(date)=?';
+
+    connection.query(years,[year],function (err,result) {
+
+        if(err)throw err;
+        const array=[];
+
+     for(var i=0;i<result.length;i++){
+            var special_date_id=result[i].special_date_id;
+
+            var special_dates='SELECT name_en as name FROM special_dates WHERE id='+mysql.escape(special_date_id);
+            connection.query(special_dates,function (err,rows) {
+                if(err) throw err;
+            array.push(rows)
+            })
+        }
+        res.json({type: true,array});
+
+    });
 
 
 });
+
+
 
 module.exports = router;
