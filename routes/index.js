a92cbf44762debfcdbcdf6221d046581eb3ec6f1
Asara <asarakumarasena94@gmail.com>
2018-12-27 10:37:39 +0530

    route file

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,94 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../config/connection');
 var mysql = require('mysql');
-var ArrayList = require('arraylist')
 
-router.get('/home/:date', function (req, res, next) {
 
-    var date = req.params.date;
-    var get_all = 'SELECT * FROM horoscopes WHERE date=' + mysql.escape(date);
+router.get('/today/:year', function (req, res, next) {
+    var year = req.params.year;
+    var array=[];
+    //var get_all = 'SELECT date,sunrise,sunset,rahu_morning_start,rahu_morning_end,rahu_night_start,rahu_night_end,suba_dishawa,maru_dishawa,description,image_url,special_date_id_1,special_date_id_2 FROM horoscopes,special_dates WHERE horoscopes.special_date_id_1=special_dates.id AND year(date)=? OR horoscopes.special_date_id_2=special_dates.id AND year(date)=?';
+    /*//var get_all = 'SELECT * FROM horoscopes WHERE year(date)='+mysql.escape(year);*/
+    async function f() {
 
-    connection.query(get_all, function (err, result) {
-        if (err) throw err;
-        var special_date_id = result[0].special_date_id;
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve("done!"), 1000)
+        });
 
-        if (special_date_id != 1) {
-            var get_special_date = 'SELECT description,image_url FROM special_dates WHERE id=' + mysql.escape(special_date_id);
-        }
-        else {
-            var get_special_date = 'SELECT description,image_url FROM special_dates WHERE id=1';
-        }
-        connection.query(get_special_date, function (err, rows) {
-            if (err) throw err;
-            res.json({type: true, rows, result});
-        })
+        let result = await promise; // wait till the promise resolves (*)
 
-    });
+        alert(result); // "done!"
+    }
 
+    f();
+
+    /*connection.query(get_all,[year,year], function (err, result) {
+      if (err) throw err;
+res.json({result});
+    /!* for(var i=0;i<result.length;i++){
+                var special_date_id_1 = result[i].special_date_id_1;
+                var special_date_id_2 = result[i].special_date_id_2;
+
+                if(special_date_id_1!=0&& special_date_id_2==0){
+
+                    var a='SELECT image_url FROM special_dates WHERE id=?';
+                connection.query(a,[special_date_id_1],function (err,row) {
+
+                    array.push(result[i]);
+                    array.push(row);
+                })
+                }
+                else if(special_date_id_1==0&& special_date_id_2!=0){
+
+                    var a='SELECT image_url FROM special_dates WHERE id=?';
+                    connection.query(a,[special_date_id_2],function (err,row) {
+
+                        array.push(result[i]);
+                        array.push(row);
+                    })
+                }
+            }*!/
+         // res.json({array});
+    });*/
 });
+
+router.get('/month_images/:month', function (req, res, next) {
+    var month = req.params.month;
+    var image = 'SELECT image_url FROM month_images WHERE month=' + mysql.escape(month);
+    connection.query(image, function (err, result) {
+        if (err) throw err;
+        res.json({image: result});
+    });
+});
+
 
 router.get('/compass/:date', function (req, res) {
     var date = req.params.date;
+    var month = (req.params.date).slice(5, 7);
+    var get_disha = 'SELECT suba_dishawa,maru_dishawa,special_date_id_1,special_date_id_2 FROM horoscopes WHERE date=' + mysql.escape(date);
 
-    var get_disha = 'SELECT suba_dishawa,maru_dishawa FROM horoscopes WHERE date=' + mysql.escape(date);
     connection.query(get_disha, function (err, result) {
         if (err) throw err;
-        res.json({type: true, result})
+        var special_date_id_1 = result[0].special_date_id_1;
+        var special_date_id_2 = result[0].special_date_id_2;
+
+        if (special_date_id_1 != 0 || special_date_id_2 != 0) {
+
+            var is_special=true;
+            var special_dates = 'SELECT description,image_url FROM special_dates WHERE id=? AND (id)!=? OR id=? AND (id)!=?';
+            connection.query(special_dates, [special_date_id_1,0, special_date_id_2,0], function (err, rows) {
+                if (err) throw err;
+                res.json({is_special,suba_disha: result, special_dates: rows});
+            })
+        } else {
+            var is_special=false;
+            var get_month = 'SELECT image_url FROM month_images WHERE month=?';
+
+            connection.query(get_month, [month], function (err, rows) {
+                if (err) throw err;
+                res.json({is_special,suba_disha: result, monthly_image: rows});
+            })
+        }
+
     })
 
 });
@@ -43,42 +98,56 @@ router.get('/calender/:year/:month', function (req, res) {
 
     var year = req.params.year;
     var month = req.params.month;
-    var dates='SELECT category_id,day(date) as day FROM horoscopes WHERE month(date)=? AND year(date)=?';
+    var dates = 'SELECT date,category_id_1,category_id_2,category_id_3,category_id_4 FROM horoscopes,special_dates WHERE horoscopes.special_date_id_1=special_dates.id AND month(date)=? AND year(date)=? OR horoscopes.special_date_id_2=special_dates.id AND month(date)=? AND year(date)=? ';
 
-    connection.query(dates,[month,year],function (err,result) {
-        if(err)throw err;
-        res.json({type: true,result});
+    connection.query(dates, [month, year,month, year], function (err, result) {
+        if (err) throw err;
+        res.json({type: true, categories: result});
     });
 });
 
-router.get('/special_dates/:year/:ln',function (req,res) {
+router.get('/special_dates/:year/:ln', function (req, res) {
 
     var year = req.params.year;
     var language = req.params.ln;
 
-    var years='SELECT special_date_id FROM horoscopes WHERE year(date)=?';
+    switch (language) {
+        case 'en':
+            var special_dates = 'SELECT date,month(date) AS month ,name_en as name FROM horoscopes,special_dates WHERE horoscopes.special_date_id_1=special_dates.id AND year(date)=? AND (special_date_id_1)!=? OR horoscopes.special_date_id_2=special_dates.id AND year(date)=? AND (special_date_id_2)!=? ';
+            break;
+        case 'si':
+            var special_dates = 'SELECT date,month(date) AS month ,name_si as name FROM horoscopes,special_dates WHERE horoscopes.special_date_id_1=special_dates.id AND year(date)=? AND (special_date_id_1)!=? OR horoscopes.special_date_id_2=special_dates.id AND year(date)=? AND (special_date_id_2)!=? ';
+            break;
+        case 'ta':
+            var special_dates = 'SELECT date,month(date) AS month ,name_ta as name FROM horoscopes,special_dates WHERE horoscopes.special_date_id_1=special_dates.id AND year(date)=? AND (special_date_id_1)!=? OR horoscopes.special_date_id_2=special_dates.id AND year(date)=? AND (special_date_id_2)!=? ';
+            break;
 
-    connection.query(years,[year],function (err,result) {
-
-        if(err)throw err;
-        const array=[];
-
-     for(var i=0;i<result.length;i++){
-            var special_date_id=result[i].special_date_id;
-
-            var special_dates='SELECT name_en as name FROM special_dates WHERE id='+mysql.escape(special_date_id);
-            connection.query(special_dates,function (err,rows) {
-                if(err) throw err;
-            array.push(rows)
-            })
-        }
-        res.json({type: true,array});
+    }
+    connection.query(special_dates, [year,0, year,0], function (err, data) {
+        groupBy("month", data);
 
     });
-
+    function groupBy(key, array) {
+        var result = [];
+        for (var i = 0; i < array.length; i++) {
+            var added = false;
+            for (var j = 0; j < result.length; j++) {
+                if (result[j][key] == array[i][key]) {
+                    result[j].items.push(array[i]);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                var entry = {items: []};
+                entry[key] = array[i][key];
+                entry.items.push(array[i]);
+                result.push(entry);
+            }
+        }
+        res.json({result});
+    }
 
 });
-
-
 
 module.exports = router;
